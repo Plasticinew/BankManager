cfc6bd72aedabb999c2304598ed98a48411a9a08
Mark Wu <mark14@mail.ustc.edu.cn>
2019-06-29 13:04:10 +0800

    demo

--- BankManager.py
+++ BankManager.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import sqlalchemy
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 app = Flask(__name__)
@@ -7,11 +8,12 @@ app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+# def connect_db():
+#     """Connects to the specific database."""
+#     # rv = sqlite3.connect(app.config['DATABASE'])
+#     # rv.row_factory = sqlite3.Row
+#     rv = engine.connect()
+#     return rv
 
 
 if __name__ == '__main__':
